740201a658fef09d1747901fa2b94467bc5a76ee
rahulnl <rahul.sharma@hiro-microdatacenters.nl>
2024-07-29 13:44:02 +0200

    delete model UI

---
+++ src/app/ui-component/model/model-delete-confirmation/model-delete-confirmation.component.ts
@@ -0,0 +1,27 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import {Model} from "../../../model/ModelInfo";
+
+@Component({
+    selector: 'app-model-delete-confirmation',
+    templateUrl: './model-delete-confirmation.component.html',
+    styleUrls: ['./model-delete-confirmation.component.scss']
+})
+export class ModelDeleteConfirmationComponent implements OnInit {
+
+    constructor(@Inject(MAT_DIALOG_DATA) public model: Model,
+                public dialogRef: MatDialogRef<ModelDeleteConfirmationComponent>) {
+    }
+
+    ngOnInit(): void {
+    }
+
+    deleteModel(): void {
+        this.dialogRef.close({data: this.model});
+    }
+
+    closeDialog(): void {
+        this.dialogRef.close();
+    }
+
+}
--- src/app/ui-component/model/model.component.ts
+++ src/app/ui-component/model/model.component.ts
@@ -1,115 +1,173 @@
-import { Component } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatTableModule } from '@angular/material/table';
-import { MatIconModule } from '@angular/material/icon';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { environment } from '../../../environments/environment.development';
-import { FormsModule } from '@angular/forms';
-import { CogFrameworkApiService } from '../../service/cog-framework-api.service';
-import { Model } from 'src/app/model/ModelInfo';
-import { DatePipe, NgIf } from '@angular/common';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { Router } from '@angular/router';
+import {Component} from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {FormsModule} from '@angular/forms';
+import {CogFrameworkApiService} from '../../service/cog-framework-api.service';
+import {Model} from 'src/app/model/ModelInfo';
+import {DatePipe, NgIf} from '@angular/common';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {Router} from '@angular/router';
 
-import { ModelUploadComponent } from '../model-upload/model-upload.component';
+import {ModelUploadComponent} from '../model-upload/model-upload.component';
+import {MatDialog} from "@angular/material/dialog";
+import {ModelDeleteConfirmationComponent} from "./model-delete-confirmation/model-delete-confirmation.component";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 const ELEMENT_DATA: Model[] = [];
 
 @Component({
-  selector: 'app-model',
-  standalone: true,
-  imports: [
-    MatCardModule,
-    MatTabsModule,
-    MatInputModule,
-    MatButtonModule,
-    MatTableModule,
-    MatIconModule,
-    MatTooltipModule,
-    FormsModule,
-    NgIf,
-    DatePipe,
-    MatProgressBarModule,
-    ModelUploadComponent,
-  ],
-  templateUrl: './model.component.html',
-  styleUrl: './model.component.scss',
+    selector: 'app-model',
+    standalone: true,
+    imports: [
+        MatCardModule,
+        MatTabsModule,
+        MatInputModule,
+        MatButtonModule,
+        MatTableModule,
+        MatIconModule,
+        MatTooltipModule,
+        FormsModule,
+        NgIf,
+        DatePipe,
+        MatProgressBarModule,
+        ModelUploadComponent,
+    ],
+    templateUrl: './model.component.html',
+    styleUrl: './model.component.scss',
 })
 export class ModelComponent {
-  loading = false;
-  displayedColumns: string[] = [
-    'id',
-    'name',
-    'creationTime',
-    'author',
-    'action',
-  ];
-  dataSource = ELEMENT_DATA;
-  modelName = '';
-  modelId = '';
+    loading = false;
+    displayedColumns: string[] = [
+        'id',
+        'name',
+        'creationTime',
+        'author',
+        'action',
+    ];
+    dataSource = ELEMENT_DATA;
+    ttDataSource = new MatTableDataSource<Model>([]);
+    modelName = '';
+    modelId = '';
 
-  constructor(
-    private cogFrameworkApiService: CogFrameworkApiService,
-    private router: Router,
-  ) {}
-
-  open(item: any): void {
-    this.router
-      .navigate(['/model-detail'], { queryParams: { id: item.id } })
-      .then((r) => {
-        console.log('redirected to other component');
-      });
-  }
-
-  search(): void {
-    if (this.modelId.length > 0) {
-      console.log('Search by ID');
-      this.searchByID();
-    } else {
-      console.log('Search by Name');
-      this.searchByName();
+    constructor(private cogFrameworkApiService: CogFrameworkApiService, private router: Router,
+                private dialog: MatDialog, private snackBar: MatSnackBar) {
     }
-  }
 
-  searchByID(): void {
-    this.loading = true;
-    const response = this.cogFrameworkApiService.getModelById(this.modelId);
+    open(item: any): void {
+        this.router
+            .navigate(['/model-detail'], {queryParams: {id: item.id}})
+            .then((r) => {
+                console.log('redirected to other component');
+            });
+    }
 
-    response.subscribe({
-      next: (v) => {
-        const model = [];
-        model.push(v.data);
-        this.dataSource = model;
-      },
-      error: (e) => {
-        console.error(e);
-        this.loading = false;
-      },
-      complete: () => {
-        this.loading = false;
-        console.info('complete');
-      },
-    });
-  }
+    search(): void {
+        if (this.modelId.length > 0) {
+            console.log('Search by ID');
+            this.searchByID();
+        } else {
+            console.log('Search by Name');
+            this.searchByName();
+        }
+    }
 
-  searchByName(): void {
-    this.loading = true;
-    const response = this.cogFrameworkApiService.getModelByName(this.modelName);
-    response.subscribe({
-      next: (v) => {
-        this.dataSource = v.data;
-      },
-      error: (e) => {
-        console.error(e);
-        this.loading = false;
-      },
-      complete: () => {
-        this.loading = false;
-        console.info('complete');
-      },
-    });
-  }
+    searchByID(): void {
+        this.loading = true;
+        const response = this.cogFrameworkApiService.getModelById(this.modelId);
+
+        response.subscribe({
+            next: (v) => {
+                const model = [];
+                model.push(v.data);
+                this.dataSource = model;
+                this.ttDataSource.data = model;
+            },
+            error: (e) => {
+                console.error(e);
+                this.loading = false;
+            },
+            complete: () => {
+                this.loading = false;
+                console.info('complete');
+            },
+        });
+    }
+
+    searchByName(): void {
+        this.loading = true;
+        const response = this.cogFrameworkApiService.getModelByName(this.modelName);
+        response.subscribe({
+            next: (v) => {
+                this.dataSource = v.data;
+            },
+            error: (e) => {
+                console.error(e);
+                this.loading = false;
+            },
+            complete: () => {
+                this.loading = false;
+                console.info('complete');
+            },
+        });
+    }
+
+    // trade table function
+    openModelDialog(model: any): void {
+        console.log(model);
+        const dialogRef = this.dialog.open(ModelDeleteConfirmationComponent, {
+            //width: '25vw',
+            //height: '20vh',
+            data: model
+        });
+        dialogRef.afterClosed().subscribe(result => {
+            console.log("after close");
+            if (result) {
+                console.log(result);
+                console.log(result.data);
+                this.deleteModelById(result.data.id)
+            }
+        })
+    }
+
+
+    deleteModelById(id: number): void {
+
+        const response = this.cogFrameworkApiService.deleteModelById(id);
+        response.subscribe({
+            next: (v) => {
+                console.log(v);
+                this.openSnackBar('Model deleted', 'Close');
+                this.deleteModelFromTable(id);
+            },
+            error: (e) => {
+                console.error(e);
+            },
+            complete: () => {
+                console.info('complete');
+            },
+        });
+    }
+
+    private openSnackBar(message: string, action: string): void {
+        this.snackBar.open(message, action, {
+            duration: 3000,
+            horizontalPosition: 'end',
+            verticalPosition: 'bottom'
+        });
+    }
+
+    private deleteModelFromTable(id: number): void {
+        const remaining = [];
+        for (const i in this.dataSource) {
+            if (id !== this.dataSource[i].id) {
+                remaining.push(this.dataSource[i]);
+            }
+        }
+        this.dataSource = remaining;
+    }
 }
