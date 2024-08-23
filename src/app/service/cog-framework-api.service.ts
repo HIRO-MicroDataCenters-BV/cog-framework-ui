a101a61cbe5a9ea878505add5449856ca0a4b843
rahulnl <rahul.sharma@hiro-microdatacenters.nl>
2024-08-23 13:10:51 +0200

    merge conflicts

--- src/app/service/cog-framework-api.service.ts
+++ src/app/service/cog-framework-api.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { GetModelParams, ModelInfo } from '../model/ModelInfo';
 import {
-  DatasetById,
+  DatasetById, DataSetData,
   DatasetInfo,
   GetDatasetParams,
   UploadedDataset,
