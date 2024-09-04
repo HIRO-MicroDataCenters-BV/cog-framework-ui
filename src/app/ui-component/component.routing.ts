14a1c0e81aaa29e1e937e8d880bf03b43a1e667f
Eugene Popov <kaaboeld@gmail.com>
2024-09-04 22:25:29 +0700

    Fix temp unused import

--- src/app/ui-component/component.routing.ts
+++ src/app/ui-component/component.routing.ts
@@ -6,7 +6,7 @@ import { ModelDetailComponent } from './model-detail/model-detail.component';
 import { DatasetDetailComponent } from './dataset-detail/dataset-detail.component';
 import { ModelValidationSearchComponent } from './model-validation-search/model-validation-search.component';
 import { ModelValidationArtifactsComponent } from './model-validation-artifacts/model-validation-artifacts.component';
-import { PipelineComponent } from './pipeline/pipeline.component';
+//import { PipelineComponent } from './pipeline/pipeline.component';
 import { ModelTrainingComponent } from './model-training/model-training.component';
 import { ModelDeploymentComponent } from './model-deployment/model-deployment.component';
 import { ModelServingComponent } from './model-serving/model-serving.component';
