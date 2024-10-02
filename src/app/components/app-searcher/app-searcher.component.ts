785bb7ddfa5cc2559789a7d59fe16973783984ee
Kagameow <kagami.404@gmail.com>
2024-10-02 17:43:10 +0200

    base component logic

--- src/app/components/app-searcher/app-searcher.component.ts
+++ src/app/components/app-searcher/app-searcher.component.ts
@@ -1,17 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { KeyValuePipe } from '@angular/common';
+import { MatOption } from '@angular/material/autocomplete';
+import { MatFormField, MatSelect } from '@angular/material/select';
+import { MatInput } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+
+export type SearcherOption = {
+  key: string;
+  label: string;
+};
+
+type SearcherEvent = {
+  key: string;
+  query: string;
+};
 
 @Component({
   selector: 'app-searcher',
   templateUrl: './app-searcher.component.html',
   standalone: true,
   styleUrls: ['./app-searcher.component.scss'],
+  imports: [
+    KeyValuePipe,
+    MatOption,
+    MatSelect,
+    MatFormField,
+    MatInput,
+    ReactiveFormsModule,
+    FormsModule,
+  ],
 })
 export class AppSearcherComponent {
-  searchValue: string = '';
+  @Output() changeEvent = new EventEmitter<SearcherEvent>();
+  @Input() options: SearcherOption[] = [
+    { key: 'name', label: 'Model Name' },
+    { key: 'id', label: 'Model Id' },
+  ];
+  key: string = this.options[0].key;
+  query: string = '';
+  private searchSubject = new Subject<void>();
 
-  constructor() {}
+  constructor() {
+    this.searchSubject.pipe(debounceTime(150)).subscribe(() => {
+      this.emitSearchEvent();
+    });
+  }
 
   search() {
-    console.log('Search value:', this.searchValue);
+    this.searchSubject.next();
+  }
+
+  private emitSearchEvent() {
+    console.log('Search value:', {
+      key: this.key,
+      query: this.query,
+    });
+    this.changeEvent.emit({ key: this.key, query: this.query });
   }
 }
