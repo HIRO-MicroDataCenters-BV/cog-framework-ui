3a3743bf4fd1b89c9e16eb9dfb5014a97c4bd82d
Kagameow <52716105+Kagameow@users.noreply.github.com>
2024-10-18 16:52:12 +0200

    fixed api endpoint (#62)

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
   appURL:
-    'http://ec2-18-191-191-251.us-east-2.compute.amazonaws.com:31181/cogapi',
-  //'http://ec2-18-191-191-251.us-east-2.compute.amazonaws.com:31181/cogapi',
+    'http://ec2-18-223-161-68.us-east-2.compute.amazonaws.com:31181/cogapi',
 };
