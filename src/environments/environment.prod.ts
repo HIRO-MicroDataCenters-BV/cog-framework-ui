bc03b7dd678ed0a3436b8e1bd4360df3e84678f5
Kagameow <kagami.404@gmail.com>
2024-10-22 14:08:33 +0200

    somehow it's breaking without defaults

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  appURL:
-    'http://ec2-18-223-161-68.us-east-2.compute.amazonaws.com:31181/cogapi',
+  appURL: 'http://dev.cog-dev.hiro-develop.nl/cogapi',
 };
