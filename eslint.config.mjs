40dd6b95024d698b3046c96136e4bd0b66c8c203
Eugene Popov <kaaboeld@gmail.com>
2025-01-20 18:56:02 +0100

    Add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,14 @@
 import { createConfigForNuxt } from "@nuxt/eslint-config/flat";
 
-export default createConfigForNuxt({});
+export default createConfigForNuxt({
+  extends: [
+    "eslint:recommended",
+    "plugin:nuxt/recommended",
+    "plugin:prettier/recommended",
+    "prettier",
+  ],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+});
