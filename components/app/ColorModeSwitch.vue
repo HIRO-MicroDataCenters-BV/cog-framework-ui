98f9ccb48d68c82375b4cb7e9a5bfaecd9cbceb5
Eugene Popov <kaaboeld@gmail.com>
2025-02-06 16:40:54 +0100

    Rename components to CamelCase

---
+++ components/app/ColorModeSwitch.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Switch :checked="isDark" @update:checked="toggleTheme">
+      <template #thumb>
+        <Icon v-if="isDark" name="lucide:moon" />
+        <Icon v-else name="lucide:sun" />
+      </template>
+    </Switch>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const colorMode = useColorMode();
+const isDark = computed(() => colorMode.value === 'dark');
+
+const toggleTheme = () => {
+  const value = isDark.value ? 'light' : 'dark';
+  colorMode.value = value;
+  colorMode.preference = value;
+};
+</script>
+
+<style></style>
