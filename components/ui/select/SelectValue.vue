50f927f894fb220dbda773e3fe0f5779147ed12a
Eugene Popov <kaaboeld@gmail.com>
2026-01-08 14:38:25 +0100

    Fix select values for long select value

--- components/ui/select/SelectValue.vue
+++ components/ui/select/SelectValue.vue
@@ -5,7 +5,7 @@ const props = defineProps<SelectValueProps>();
 </script>
 
 <template>
-  <SelectValue data-slot="select-value" v-bind="props">
+  <SelectValue data-slot="select-value" v-bind="props" class="max-w-[150px] overflow-hidden text-ellipsis">
     <slot />
   </SelectValue>
 </template>
