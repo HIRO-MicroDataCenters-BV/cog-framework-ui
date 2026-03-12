728a59989e70e2ba3ed6ef78246dfe54e50acfc7
rahulnl <rahul.sharma@hiro-microdatacenters.nl>
2026-03-12 14:25:51 +0100

    flowise

---
+++ pages/flowise.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const { setPage } = useApp();
+
+setPage({
+  section: 'flowise',
+});
+
+const flowiseUrl = '/chat/?chatflowid=1';
+</script>
+
+<template>
+  <div class="h-[calc(100svh-74px)] w-full">
+    <iframe
+      :src="flowiseUrl"
+      title="Flowise"
+      class="w-full h-full border-0"
+      referrerpolicy="no-referrer"
+    />
+  </div>
+</template>
+
